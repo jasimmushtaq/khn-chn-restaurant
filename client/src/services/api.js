8b1b41bb91711be5ed44a55be2e8e06f88218691
Jasim Mushtaq <jasimmushtaq786.com>
2026-03-09 20:54:23 +0530

    update server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://khn-chn-restaurant-backend.onrender.com',
+    baseURL: 'http://localhost:5000/api',
 });
 
 // Add JWT token to every request (admin or delivery)
